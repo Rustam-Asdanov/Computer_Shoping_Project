a9b5d1e86f6e85faa05fa885ea0a3997df319842
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2021-11-22 12:57:22 +0400

    Login page is done Registration page is done Main page is done

---
+++ src/main/java/com/project/full/ComputerShoping/ComputerShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.project.full.ComputerShoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerShoppingApplication.class, args);
+	}
+
+}
