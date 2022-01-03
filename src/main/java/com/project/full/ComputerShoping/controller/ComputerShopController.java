24522408c7c6fce0cb7e3e27c9dabbd621ee38eb
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-01-03 12:22:09 +0400

    project is ready except some issues with photo

--- src/main/java/com/project/full/ComputerShoping/controller/ComputerShopController.java
+++ src/main/java/com/project/full/ComputerShoping/controller/ComputerShopController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +20,14 @@ public class ComputerShopController {
         model.addAttribute("computer_info_list",
                 computerDaoService.getComputerListForShop(0));
         return "/computer_shop";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String getDetailPage(
+            @PathVariable("id") long id,
+            Model model
+    ){
+        model.addAttribute("comp",computerDaoService.getComputerById(id));
+        return "computer_detail";
     }
 }
